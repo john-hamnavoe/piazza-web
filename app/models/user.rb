026a8fb84c02f862d99f1fb5eb5126ab6e2c4c82
john-hamnavoe <john.wallace@hamnavoepartners.com>
2023-03-16 17:29:41 +0000

    Build the authentication system

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :memberships, dependent: :destroy
   has_many :organizations, through: :memberships
+  has_many :app_sessions, dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_senstive: true}, format: { with: URI::MailTo::EMAIL_REGEXP }
@@ -8,6 +9,20 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 8, maximum: ActiveModel::SecurePassword::MAX_PASSWORD_LENGTH_ALLOWED }
 
   before_validation :strip_extraneous_spaces
+
+  def self.create_app_session(email:, password:)
+    return nil unless user = User.find_by(email: email.downcase)
+  
+    user.app_sessions.create if user.authenticate(password)
+  end  
+
+  def authenticate_app_session(app_session_id, token)
+    app_sessions.find(app_session_id).authenticate_token(token)
+    
+    rescue ActiveRecord::RecordNotFound
+    
+    nil
+  end  
 
   private
 
